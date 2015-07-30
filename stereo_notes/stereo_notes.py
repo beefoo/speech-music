cdf0c1b8bfa7cffc921fbc762ed2f50d8f19a881
beefoo <brian@youaremyjoy.org>
2015-07-30 15:20:16 -0400

    and sequence output and chuck script to stereo notes

--- stereo_notes/stereo_notes.py
+++ stereo_notes/stereo_notes.py
@@ -1,6 +1,8 @@
-import sys
-import csv
 from aubio import source, pitch, freqtomidi
+import csv
+import os
+import sys
+import time
 
 if len(sys.argv) < 1:
     print "Usage: %s <inputfile>" % sys.argv[0]
@@ -8,6 +10,8 @@ if len(sys.argv) < 1:
 
 inputFile = sys.argv[1]
 inputFilename = inputFile.split('/')[-1].split('.')[0]
+instrumentFile = "data/instruments.csv"
+instrumentDir = "instruments/"
 
 writeNotes = True
 writeSequence = True
@@ -15,9 +19,11 @@ writeSequence = True
 sampleRate = 44100
 tolerance = 0.8
 downsample = 1
-minOctave = 1
-maxOctave = 6
-minNoteDuration = 10
+minNoteDuration = 1
+gain = 1.0
+
+minOctave = None
+maxOctave = None
 
 win_s = 4096 / downsample # fft size
 hop_s = 512 / downsample # hop size
@@ -29,6 +35,7 @@ pitch_o.set_unit("freq")
 pitch_o.set_tolerance(tolerance)
 
 notes = []
+instruments = []
 sequence = []
 
 # Convert midi to note
@@ -67,9 +74,31 @@ while True:
     total_frames += read
     if read < hop_s: break
 
+# Read instrument data
+with open(instrumentFile, 'rb') as f:
+    r = csv.reader(f, delimiter=',')
+    next(r, None) # remove header
+    for file,note,octave,duration in r:
+        instruments.append({
+            'index': len(instruments),
+            'file': instrumentDir + file,
+            'note': note,
+            'octave': int(octave),
+            'note_octave': note + octave,
+            'duration': int(duration)
+        })
+    instruments = sorted(instruments, key=lambda k: k['duration'])
+    minOctave = min([i['octave'] for i in instruments])
+    maxOctave = max([i['octave'] for i in instruments])
+
+def selectInstrument(note_octave, duration):
+    global instruments
+    matches = [i for i in instruments if i['note_octave']==note_octave and i['duration'] < duration]
+    return matches[-1]
+
 def addQueueToSequence(q):
-    global sequence
     global minNoteDuration
+    global sequence
 
     n0 = q[0]
     n1 = q[-1]
@@ -79,7 +108,8 @@ def addQueueToSequence(q):
             'ms': n0['ms'],
             'duration': queue_duration,
             'note': n0['note'],
-            'octave': n0['octave']
+            'octave': n0['octave'],
+            'instrument': selectInstrument(n0['note_octave'], queue_duration)
         })
 
 # Build sequence
@@ -97,6 +127,10 @@ for n in notes:
 if len(queue) > 0:
     addQueueToSequence(queue)
 
+total_ms = sequence[-1]['ms']
+total_seconds = int(1.0*total_ms/1000)
+print('Main sequence time: '+time.strftime('%M:%S', time.gmtime(total_seconds)) + ' (' + str(total_seconds) + 's)')
+
 # Write notes to file
 if writeNotes:
     with open('output/'+inputFilename+'-notes.csv', 'wb') as f:
@@ -107,8 +141,26 @@ if writeNotes:
 
 # Write sequence to file
 if writeSequence:
-    with open('output/'+inputFilename+'-sequence.csv', 'wb') as f:
+    # Write human-readable sequence
+    with open('output/'+inputFilename+'-sequence-readable.csv', 'wb') as f:
         w = csv.writer(f)
         for s in sequence:
             w.writerow([s['ms'], s['duration'], s['note'], s['octave']])
-        print('Successfully wrote '+str(len(sequence))+' notes to sequence file')
+    # Write instruments
+    with open('output/'+inputFilename+'-instruments.csv', 'wb') as f:
+        w = csv.writer(f)
+        for i in instruments:
+            w.writerow([i['index']])
+            w.writerow([i['file']])
+        f.seek(-2, os.SEEK_END) # remove newline
+        f.truncate()
+    # Write machine-readable sequence
+    with open('output/'+inputFilename+'-sequence.csv', 'wb') as f:
+        w = csv.writer(f)
+        for s in sequence:
+            w.writerow([s['instrument']['index']])
+            w.writerow([gain])
+            w.writerow([s['ms']])
+        f.seek(-2, os.SEEK_END) # remove newline
+        f.truncate()
+    print('Successfully wrote '+str(len(sequence))+' notes to sequence file')
